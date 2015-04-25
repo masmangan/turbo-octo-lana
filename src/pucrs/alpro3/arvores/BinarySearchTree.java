6fb56b5e19b9ba1be9b7eb73caeeae3779177006
Marco Aurelio Souza Mangan <10067320@l1670629.portoalegre.pucrsnet.br>
2015-04-24 21:33:25 -0300

    implementação inicial

--- src/pucrs/alpro3/arvores/BinarySearchTree.java
+++ src/pucrs/alpro3/arvores/BinarySearchTree.java
@@ -2,4 +2,32 @@ package pucrs.alpro3.arvores;
 
 public class BinarySearchTree {
 
+	class Node {
+		int value;
+		Node left, right;
+	}
+	
+	private Node root;
+	
+	public BinarySearchTree() {
+		root = null;
+	}
+	
+	public boolean isEmpty() {
+		return root == null;
+	}
+	
+	public int size() {
+		// TODO
+		return 0;
+	}
+
+	public void add(int value) {
+		// TODO
+	}
+	
+	public boolean contains(int value) {
+		// TODO
+		return false;
+	}	
 }
